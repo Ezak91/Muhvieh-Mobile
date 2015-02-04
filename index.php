584aa21d89132af1d81c3d4e927bdf902be8b103
Ezak91 <marc.szymkowiak91@googlemail.com>
2015-02-04 09:58:03 +0100

    Add jquery files to local directory

--- index.php
+++ index.php
@@ -8,10 +8,10 @@ session_start();
 	<meta charset="UTF-8" />
 	<meta name="Marc Szymkowiak" content="http://muhvieh.ezak91.de/" />
 	<meta name="viewport" content="width=device-width, initial-scale=1,maximum-scale=1" />
-	<link rel="stylesheet" href="//ajax.googleapis.com/ajax/libs/jqueryui/1.11.0/themes/smoothness/jquery-ui.css" />	
-	<link rel="stylesheet" href="http://code.jquery.com/mobile/1.4.2/jquery.mobile-1.4.2.min.css" />
-	<script  src="http://code.jquery.com/jquery-1.9.1.min.js"></script>
-	<script  src="http://code.jquery.com/mobile/1.4.2/jquery.mobile-1.4.2.min.js"></script>
+	<link rel="stylesheet" href="css/jquery-ui.css" />	
+	<link rel="stylesheet" href="css/jquery.mobile-1.4.2.min.css" />
+	<script  src="jquery/jquery-1.9.1.min.js"></script>
+	<script  src="jquery/jquery.mobile-1.4.2.min.js"></script>
 </head>		
 <body>	
 	<div data-role="page">
