afe638ea286495d6aae64f138f2ae7a9cb370ce3
Ezak91 <marc.szymkowiak91@googlemail.com>
2015-02-04 10:22:01 +0100

    Use correct css file for jquery mobile

--- index.php
+++ index.php
@@ -9,7 +9,7 @@ session_start();
 	<meta name="Marc Szymkowiak" content="http://muhvieh.ezak91.de/" />
 	<meta name="viewport" content="width=device-width, initial-scale=1,maximum-scale=1" />
 	<link rel="stylesheet" href="css/jquery-ui.css" />	
-	<link rel="stylesheet" href="css/jquery.mobile-1.4.2.min.css" />
+	<link rel="stylesheet" href="css/jquery.mobile-1.4.5.min.css" />
 	<script  src="jquery/jquery-1.11.2.min.js"></script>
 	<script  src="jquery/jquery.mobile-1.4.5.min.js"></script>
 </head>		
