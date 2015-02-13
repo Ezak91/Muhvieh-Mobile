54706f516c5e4c3be808004cbc2e770d9839ca47
Ezak <marc.szymkowiak91@googlemail.com>
2015-02-13 17:03:46 +0100

    Add popup to covers

--- includes/movieinfo.php
+++ includes/movieinfo.php
@@ -10,15 +10,20 @@ if(isset($_SESSION["user_id"]))
 	$abfrage = "SELECT * FROM movies where id=$id";
 	$ergebnis = mysql_query($abfrage);
 	$row = mysql_fetch_object($ergebnis);
+	$cover = $row->cover;
 
 	echo"
 
 		<div data-role='header' data-theme='a'>
 		<h1>$row->title</h1>
 		</div>
-		<div>
+		<div>	
 			<ul data-role='listview' data-inset='true'>
-			<span style='float: right;'><img src = 'http://image.tmdb.org/t/p/w500$row->cover' style='width:40%'></span><br>
+			<span style='float: right;'>
+				<a href='#popupcover' data-rel='popup' data-position-to='window' data-transition='fade'>
+					<img src = 'http://image.tmdb.org/t/p/w500$row->cover' style='width:40%'>
+				</a>
+			</span><br>	
 			<li>Titel: <b>$row->title</b></li>
 			<li>Original Titel: <b>$row->original_title</b></li>
 			<li>Veröffentlicht am: <b>$row->release_date</b></li>
@@ -56,6 +61,9 @@ if(isset($_SESSION["user_id"]))
 				echo"<li><iframe width=\"425\" height=\"350\" src=\"//www.youtube.com/embed/$row->trailer_id?rel=0\" frameborder=\"0\" allowfullscreen></iframe></li>";
 			}
 			echo "</ul></div>";
+			echo "<div data-role='popup' id='popupcover' data-overlay-theme='a' data-theme='d' data-corners='false'>
+                  <a href='#' data-rel='back' data-role='button' data-theme='b' data-icon='delete' data-iconpos='notext' class='ui-btn-right'>Close</a><img src='http://image.tmdb.org/t/p/w500$cover' style='max-height:512px;'' alt='$cover' />
+                  </div>";
 
 }
 else
