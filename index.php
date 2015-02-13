0fca2aeaec3fe938d875690bc5f191120c5b21d8
Ezak <marc.szymkowiak91@googlemail.com>
2015-02-13 15:56:41 +0100

    Show full informations on small displays

--- index.php
+++ index.php
@@ -10,6 +10,7 @@ session_start();
 	<meta name="viewport" content="width=device-width, initial-scale=1,maximum-scale=1" />
 	<link rel="stylesheet" href="css/jquery-ui.css" />	
 	<link rel="stylesheet" href="css/jquery.mobile-1.4.5.min.css" />
+	<link rel="stylesheet" href="css/muhvieh.css">
 	<script  src="jquery/jquery-1.11.2.min.js"></script>
 	<script  src="jquery/jquery.mobile-1.4.5.min.js"></script>
 </head>		
